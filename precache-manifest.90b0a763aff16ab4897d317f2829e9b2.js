73e1bbe712c4faeba47f5d204867fa7a127a608c
hank.h <villiard+githank@gmail.com>
2019-07-19 00:43:21 +0800

    Updates

---
+++ precache-manifest.90b0a763aff16ab4897d317f2829e9b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5347df8512ad82dd14049f0635bb3f9b",
+    "url": "/randomquote/index.html"
+  },
+  {
+    "revision": "ab956065f4ebb0929f91",
+    "url": "/randomquote/static/js/2.f868f751.chunk.js"
+  },
+  {
+    "revision": "a82437487ac9c1035389",
+    "url": "/randomquote/static/js/main.675f9948.chunk.js"
+  },
+  {
+    "revision": "a6f183b792bdc713ca91",
+    "url": "/randomquote/static/js/runtime~main.75f6ba41.js"
+  }
+]);
